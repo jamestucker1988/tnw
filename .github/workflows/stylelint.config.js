1fb9500b9a17c8ec343b0b6fbfb228ccc6f68482
jamestucker1988 <xat2212@gmail.com>
2020-07-04 08:20:36 +0530

    fixed style.scss errors

--- .github/workflows/stylelint.config.js
+++ .github/workflows/stylelint.config.js
@@ -106,4 +106,5 @@ module.exports = {
         "value-list-comma-space-before": "never",
         "value-list-max-empty-lines": 0,
     },
+    ignoreAtRules: ['function', 'if','include','mixin']
 }
